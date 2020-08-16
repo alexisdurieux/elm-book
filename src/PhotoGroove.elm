b16ee52490ef728b52726d130af3317a8954b076
Alexis Durieux <alexis.durieux94@gmail.com>
2020-08-16 12:31:02 +0200

    end of your first elm application

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,16 +2,55 @@ module PhotoGroove exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Browser
+
+baseUrl : String
+baseUrl = "http://elm-in-action.com/"
+
+thumbnail : String -> String -> Html { data : String, description : String }
+thumbnail selected thumb = 
+    img 
+        [ src (baseUrl ++ thumb) 
+        , classList [ ("selected", selected == thumb )]
+        , onClick { description = "ClickedPhoto", data = thumb }
+        ] 
+        []
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selected = msg.data }
+    else
+        model
 
 view model =
     div [ class "content"]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        , div [ id "thumbnails" ] 
+            (List.map 
+                (thumbnail model.selected)
+                model.photos
+            )
+        , img 
+            [ class "large"
+            , src (baseUrl ++ "large/" ++ model.selected)
             ]
+            []
         ]
 
+initialModel = 
+    {
+        photos = [
+            "1.jpeg"
+            , "2.jpeg"
+            , "3.jpeg"
+        ]
+        , selected = "1.jpeg"
+    }
+
 main =
-    view "no model yet"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
